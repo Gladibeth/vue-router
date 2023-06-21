fdd38d3fd843fcc1a48954c0fd253bb7785cc97b
Gladibeth <gladibethdelosa@gmail.com>
2023-06-20 21:51:48 -0500

    Change load router componets with ladyload (router,components)

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-link to="/">Pokemon List</router-link>
+    <router-link to="/id">Pokemon por id</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+
+<style  scoped>
+  div{
+    padding: 0 30px
+  }
+
+  div a{
+    color: #2c3e50;
+    font-weight: 600;
+    margin: 0 10px;
+  }
+</style>
