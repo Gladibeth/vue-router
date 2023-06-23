7734a250f09e761db06766901927998a163ec8c4
Gladibeth <gladibethdelosa@gmail.com>
2023-06-22 20:13:44 -0500

    URL segments and query parameters

--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,8 @@ const routes = [
     component: () => import( /* webpackChunkName: "AboutPage"*/'@/modules/pokemon/pages/AboutPage'
   )},
   { 
-    path: '/id', 
+    path: '/:id', 
+    name: 'pokemonid', 
     component: () => import( /* webpackChunkName: "PokemonPage" */'@/modules/pokemon/pages/PokemonPage'
   )},
   { 
