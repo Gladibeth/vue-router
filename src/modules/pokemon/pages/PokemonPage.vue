51a0e0267b08afacae75f3167fced43f6de36bb1
Gladibeth <gladibethdelosa@gmail.com>
2023-06-27 18:01:03 -0500

    HTTP request and redirects

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,6 +1,9 @@
 <template>
   <!-- <h1>Pokemon Page <span>{{ this.$route.params.id }}</span></h1> -->
   <h1>Pokemon Page <span>{{ id }}</span></h1>
+  <div v-if="pokemon">
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+  </div>
 </template>
 
 <script>
@@ -14,6 +17,7 @@ export default {
   data(){
     return{
       //id: this.$route.params.id,
+      pokemon: null,
     }
   },
   created(){
@@ -21,6 +25,24 @@ export default {
     /* const { id } = this.$route.params
     console.log(id)
     this.id = id; */
+
+    this. getPokemon()
+  },
+  methods:{
+    async getPokemon(){
+      try{  
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then(result => result.json())
+        console.log(pokemon)
+        this.pokemon = pokemon
+      }catch(e){
+        console.log(e.message)
+      }
+    }
+  },
+  watch:{
+    id(){
+      this.getPokemon()
+    }
   }
 }
 </script>
--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link to="/">Pokemon List</router-link>
-    <router-link to="/id">Pokemon por id</router-link>
+    <router-link :to="{ name: 'pokemonid', params: {id: 150}}">Pokemon por id</router-link>
     <router-link to="/about">About</router-link>
   </div>
 </template>
